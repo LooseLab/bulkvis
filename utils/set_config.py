1e5682768a8fb7e549a04e01094cf0b1a7a2bbd8
Alex Payne <alexander.payne@nottingham.ac.uk>
2018-04-30 13:18:42 +0100

    removed unused methods

--- utils/set_config.py
+++ utils/set_config.py
@@ -19,7 +19,6 @@ def get_args():
     in_args.add_argument("-b", "--bulkfile",
                          help="A bulk-fast5 file to get labels from",
                          type=str,
-                         default="",
                          required=True,
                          metavar=''
                          )
@@ -46,13 +45,6 @@ def get_args():
                           )
     return parser.parse_args()
 
-def str2bool(v):
-    if v.lower() in ('true', 't', 'yes', 'y', '1'):
-        return True
-    elif v.lower() in ('false', 'f', 'no', 'n', '0'):
-        return False
-    else:
-        return ArgumentTypeError('Boolean value expected.')
 
 def get_annotations(path, enum_field):
     data_dtypes = {}
