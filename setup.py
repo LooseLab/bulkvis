22e0a22e8cb470ddf4dc3339f5ecf5482c7a5e21
Alex Payne <alexander.payne@nottingham.ac.uk>
2020-06-21 20:25:30 +0100

    reorganise for 2.0

--- setup.py
+++ setup.py
@@ -4,10 +4,19 @@ version = {}
 with open("bulkvis/_version.py") as fh:
     exec(fh.read(), version)
 
+install_requires = [
+    "bokeh~=2.1.0",
+    "h5py~=2.10.0",
+    "pandas~=1.0.5",
+    "tornado~=6.0.4",
+    "tqdm~=4.46.1",
+]
+
 setup(
     name="bulkvis",
     version=version["__version__"],
     author="Alexander Payne",
+    install_requires=install_requires,
     entry_points={
         'console_scripts': [
             'bulkvis=bulkvis.bulkvis:main',
